14ed845b59f6e70727080b13e760d94a0be8f7ff
dfriedman-tabor <dfriedman@taboracademy.org>
2025-09-19 12:53:51 -0400

    Add files via upload

---
+++ inheritance/Ball.java
@@ -0,0 +1,30 @@
+package inheritance;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball {
+
+	int x, y, speedx, speedy, size;
+	Color color;
+	
+	public Ball(int x, int y, int speedx, int speedy, int size, Color c) {
+		this.x = x;
+		this.y = y;
+		this.speedx = speedx;
+		this.speedy = speedy;
+		color = c;
+		this.size = size;
+	}
+	
+	public void move() {
+		x += speedx;
+		y += speedy;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(color);
+		g.fillOval(x, y, size, size);
+	}
+	
+}
